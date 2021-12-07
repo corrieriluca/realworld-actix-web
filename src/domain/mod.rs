9cb68234b4769889bd4ad6c837c46e2c9bb493ec
Luca Corrieri <luca.corrieri@epita.fr>
2021-12-07 14:22:31 +0000

    style(api-users): add doc comments for domain module

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,1 +1,4 @@
+//! This module deals with the bussiness logic of the application
+//! (e.g. input validation).
+
 pub mod users;
--- src/domain/users/mod.rs
+++ src/domain/users/mod.rs
@@ -1,3 +1,5 @@
 pub mod email;
 pub mod new_user;
 pub mod username;
+
+pub use new_user::NewUser;
