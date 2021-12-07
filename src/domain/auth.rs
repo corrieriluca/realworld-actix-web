0b21dd6613fced99eb48ce00f526f663f8193f97
Luca Corrieri <luca.corrieri@epita.fr>
2021-12-07 23:26:47 +0100

    fix(clippy): fix clippy warnings on needless lifetimes

--- src/domain/auth.rs
+++ src/domain/auth.rs
@@ -3,13 +3,13 @@ use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
 use crate::models::auth::Claims;
 
 /// Create a Claims struct for a new JWT (valid for one hour)
-fn create_claims_from_user<'a>(username: &'a str) -> Claims<'a> {
+fn create_claims_from_user(username: &str) -> Claims {
     let iat = time::OffsetDateTime::now_utc();
     let exp = iat + time::Duration::hours(1);
 
     Claims::new(
         "conduit-v1",
-        &username,
+        username,
         "conduit-v1",
         exp.unix_timestamp() as u64,
         iat.unix_timestamp() as u64,
@@ -17,8 +17,8 @@ fn create_claims_from_user<'a>(username: &'a str) -> Claims<'a> {
 }
 
 /// Create a JWT for the specified user (valid for one hour).
-pub fn create_jwt_for_user<'a>(
-    username: &'a str,
+pub fn create_jwt_for_user(
+    username: &str,
     shared_secret: &str,
 ) -> Result<String, jsonwebtoken::errors::Error> {
     let claims = create_claims_from_user(username);
