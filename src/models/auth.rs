2e77b358e9edc115bf6c75fdf4c47b169a051d63
Luca Corrieri <luca.corrieri@epita.fr>
2021-12-08 15:23:29 +0000

    refactor(api-users): move JwtSecret struct in models module

--- src/models/auth.rs
+++ src/models/auth.rs
@@ -1,5 +1,12 @@
+//! This module contains several strutures dealing with authentication
+//! with a JSON Web Token (JWT).
+
 use serde::{Deserialize, Serialize};
 
+/// Just a wrapper around a string that holds a JWT shared secret.
+pub struct JwtSecret(pub String);
+
+/// The structure defining the payload part of a JWT.
 #[derive(Serialize, Deserialize)]
 pub struct Claims<'a> {
     iss: &'static str,
