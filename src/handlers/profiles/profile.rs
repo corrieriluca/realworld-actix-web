e98e6a7e26d3d8a7328be8c35018926efa2c2786
Luca Corrieri <luca.corrieri@epita.fr>
2022-02-14 16:03:56 +0100

    feat(api-profiles): use MaybeAuthenticatedUser

--- src/handlers/profiles/profile.rs
+++ src/handlers/profiles/profile.rs
@@ -1,14 +1,20 @@
 use actix_web::{get, web, HttpResponse};
-use serde::Deserialize;
 
-#[derive(Deserialize)]
-struct GetProfileRequest {
-    username: String,
-}
+use crate::middlewares;
 
 /// The `GET /api/profiles/:username` endpoint.
 /// TODO.
 #[get("/{username}")]
-async fn get_profile(req: web::Path<GetProfileRequest>) -> HttpResponse {
-    HttpResponse::Ok().body(format!("hello {}", req.username))
+async fn get_profile(
+    username: web::Path<String>,
+    user: middlewares::MaybeAuthenticatedUser,
+) -> HttpResponse {
+    // Behave differently if authenticated.
+    match user.inner() {
+        Some(u) => HttpResponse::Ok().body(format!(
+            "Hello {}... you are looking at {username}.",
+            u.user.username
+        )),
+        None => HttpResponse::Ok().body(format!("Hello stranger! You are looking at {username}.")),
+    }
 }
