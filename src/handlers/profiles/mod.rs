528199a002491a075db868f25363bcc008cde420
Luca Corrieri <luca.corrieri@epita.fr>
2022-02-15 11:39:31 +0100

    feat(api-profiles): DELETE /api/profiles/:username/follow endpoint

--- src/handlers/profiles/mod.rs
+++ src/handlers/profiles/mod.rs
@@ -11,10 +11,11 @@ use actix_web::web;
 
 pub mod follow;
 pub mod profile;
-// pub mod unfollow;
+pub mod unfollow;
 
 /// Configure the Profiles service (`/api/profiles/...` endpoints).
 pub fn config_profiles(cfg: &mut web::ServiceConfig) {
     cfg.service(profile::get_profile);
     cfg.service(follow::follow_user);
+    cfg.service(unfollow::unfollow_user);
 }
