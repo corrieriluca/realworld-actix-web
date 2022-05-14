cee5555c11f39a5883913f97b7ecaa55f76bbffa
Luca Corrieri <luca.corrieri@epita.fr>
2022-05-14 16:37:45 +0200

    feat(config): multi-layer configuration files

--- src/configuration.rs
+++ src/configuration.rs
@@ -62,17 +62,59 @@ impl DatabaseSettings {
     }
 }
 
-/// Tries to read the `configuration.yml` file in the current
-/// working directory and returns the configuration as a [`AppSettings`].
+/// Retrieves the configurations in the `configuration` directory.
+/// The configuration to choose (either `local` or `production`) is read from
+/// the `APP_ENVIRONMENT` environment variable.
+/// Returns the configuration as a [`AppSettings`].
 /// It reads also from the environment, and thus it can override settings
 /// specified in the YAML file.
-/// For example `CONDUIT_DATABASE__PASSWORD=password` would set the
+/// For example `CONDUIT__DATABASE__PASSWORD=password` would set the
 /// `AppSettings.database.password` field.
 pub fn read_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory.");
+    let config_dir = base_path.join("configuration");
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
     let settings = config::Config::builder()
-        .add_source(config::File::with_name("configuration"))
+        .add_source(config::File::from(config_dir.join("base"))) // Base configuration
+        .add_source(config::File::from(config_dir.join(environment.as_str()))) // Environment-specific configuration
         .add_source(config::Environment::with_prefix("CONDUIT").separator("__"))
         .build()?;
 
     settings.try_deserialize()
+}
+
+/// Defines the environment (thus the corresponding configuration file) in which
+/// the app runs.
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
 }
