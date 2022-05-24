f5a125bcd86074c8c4eb37d6178781126ef9595e
Luca Corrieri <luca.corrieri@epita.fr>
2022-05-24 20:14:49 +0200

    fix(docker): create DB in dev script

--- scripts/dev_env.sh
+++ scripts/dev_env.sh
@@ -52,6 +52,7 @@ done
 >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
 sqlx migrate run
 
 >&2 echo "Postgres has been migrated, ready to go!"
