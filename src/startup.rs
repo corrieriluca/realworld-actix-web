cbde7e6ec8c7bb505461e6d90368faca576a0c18
Luca Corrieri <luca.corrieri@epita.fr>
2022-06-19 12:38:28 +0200

    chore(deps): update dependencies for the last time

--- src/startup.rs
+++ src/startup.rs
@@ -55,7 +55,7 @@ impl Application {
 /// Get a connection pool to the database specified in the given settings.
 pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
     PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
+        .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.with_db())
 }
 
